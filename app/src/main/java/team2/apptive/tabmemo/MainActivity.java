b7e452b63517d9b3680b6359997f810a4375e9aa
DarrenKimBat <uiucpass@gmail.com>
2016-11-30 14:37:56 +0900

    메시지 박스 디자인 대거 변경
    커스텀 다이얼로그 널 포인터 오류  해결
    
    저장 안됨 DB연결필요

--- app/src/main/java/team2/apptive/tabmemo/MainActivity.java
+++ app/src/main/java/team2/apptive/tabmemo/MainActivity.java
@@ -1,8 +1,10 @@
 package team2.apptive.tabmemo;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.database.Cursor;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -17,7 +19,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.InputType;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -36,24 +41,29 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-	CustomDialog mCustomDialog;
+	private Dialog mMainDialog;
 	private final long FINISH_INTERVAL_TIME = 2000;
 	private long backPressedTime = 0;
 	private Fragment listFragment = null;
 	private DBHelper dbHelper = null;
-	private EditText input = null;
 	private Button addButton = null;
-	private ListView categoryListView = (ListView) findViewById(R.id.navigation_list);
-	private ArrayList<String> items = new ArrayList<>();
-	private ArrayList<String> color_buttons = new ArrayList<>();
-	private ArrayAdapter adapter = new ArrayAdapter(addButton.getContext(), android.R.layout.simple_list_item_1, items);
+	ListView categoryListView;
+	ArrayList<String> items = new ArrayList<>();
+	ArrayList<String> color_buttons = new ArrayList<>();
+	ArrayAdapter<String> adapter;
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		setTitle("");
 
+		categoryListView = (ListView) findViewById(R.id.navigation_list);
+
+		adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, items);
 		// listview fragment
+		categoryListView.setAdapter(adapter);
+
+
 		listFragment = showFragment(ListFragment.newInstance());
 		// Open DB
 		dbHelper = new DBHelper(getApplicationContext(), "Memo.db", null, 1);
@@ -86,9 +96,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 		});
 		// Button (add new category)
 		 addButton = (Button) findViewById(R.id.navigation_button);
-		input = (EditText)findViewById(R.id.Messagebox_edit);
-		// category items
 
+		// category items
 
 		// from db
 		makeItemsForCategoryList(items);
@@ -96,6 +105,108 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 	}
 
+	public void onClickView(View v){
+		switch (v.getId()) {
+			case R.id.navigation_button:
+				mMainDialog = createDialog();
+				WindowManager.LayoutParams wm = new WindowManager.LayoutParams();
+				wm.copyFrom(mMainDialog.getWindow().getAttributes());
+				wm.height=225;
+				mMainDialog.getWindow().setGravity(Gravity.TOP);
+				mMainDialog.show();
+				break;
+		}
+	}
+
+	private AlertDialog createDialog() {
+		final View innerView = getLayoutInflater().inflate(R.layout.category_add_message_box, null);
+		AlertDialog.Builder ab = new AlertDialog.Builder(this);
+		ab.setView(innerView);
+
+		mMainDialog = ab.create();
+
+		final EditText input = (EditText)innerView.findViewById(R.id.Messagebox_edit);
+		Button right_bt = (Button)innerView.findViewById(R.id.bt_right);
+		Button left_bt = (Button)innerView.findViewById(R.id.bt_left);
+		RadioGroup Colgroup = (RadioGroup)findViewById(R.id.color_radio);
+		Colgroup.setOnCheckedChangeListener(mRadioCheck);
+
+
+
+		right_bt.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				int result = 0;
+				String value = input.getText().toString();
+				System.out.println(value+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				String non = "";
+				result = value.compareTo(non);
+				if (result == 0) {
+					Toast.makeText(getApplicationContext(), "제대로좀 쳐라", Toast.LENGTH_SHORT).show();//					mCustomDialog.dismiss();
+				}
+				else{;
+					System.out.println(value+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					items.add(" # " + value);
+					adapter.notifyDataSetChanged();
+					setDismiss(mMainDialog);
+				}
+			}
+
+		});
+
+		left_bt.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				setDismiss(mMainDialog);
+			}
+		});
+//		ab.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+//			@Override
+//			public void onClick(DialogInterface arg0, int arg1) {
+//				setDismiss(mMainDialog);
+//			}
+//		});
+//
+//		ab.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+//			@Override
+//			public void onClick(DialogInterface arg0, int arg1) {
+//				setDismiss(mMainDialog);
+//			}
+//		});
+
+		return  ab.create();
+	}
+
+	RadioGroup.OnCheckedChangeListener mRadioCheck =
+		new RadioGroup.OnCheckedChangeListener(){
+			public void onCheckedChanged(RadioGroup group, int checkedId){
+				if(group.getId() == R.id.color_radio){
+					switch (checkedId){
+						case R.id.first_col:
+							break;
+						case R.id.second_col:
+							break;
+						case R.id.third_col:
+							break;
+						case R.id.fourth_col:
+							break;
+						case R.id.fifth_col:
+							break;
+						case R.id.sixth_col:
+							break;
+						case R.id.seventh_col:
+							break;
+						case R.id.eight_col:
+							break;
+					}
+				}
+			}
+		};
+
+	private void setDismiss(Dialog dialog){
+		if(dialog!=null&&dialog.isShowing())
+			dialog.dismiss();
+	}
 
 	private Fragment showFragment(Fragment fragment) {
 		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -148,50 +259,54 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 				Toast.makeText(getApplicationContext(), "종료하려면 한 번더 !", Toast.LENGTH_LONG).show();
 			}
 		}
-
-	}
-
-	public void onClickView(View v) {
-		switch (v.getId()) {
-			case R.id.navigation_button:
-				mCustomDialog = new CustomDialog(this,
-					leftClickListener,
-					rightClickListener);
-					mCustomDialog.show();
-				break;
-		}
 	}
 
 
-	private View.OnClickListener leftClickListener = new View.OnClickListener() {
-		public void onClick(View v) {
-			int result = 0;
-			String value =  input.getText().toString();
-			String non = "";
-			result = value.compareTo(non);
-			while (true) {
-				if (result == 0) {
-					Toast.makeText(getApplicationContext(), "제대로좀 쳐라", Toast.LENGTH_SHORT).show();
-					mCustomDialog.dismiss();
-					break;
-				}
-				//공백이 아닐 때 처리할 내용
-				if (value != null) {
-					items.add(" # " + value);
-					adapter.notifyDataSetChanged();
-					mCustomDialog.dismiss();
-					break;
-				}
-			}
-		}
-	};
 
-	private View.OnClickListener rightClickListener = new View.OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			mCustomDialog.dismiss();
-		}
-	};
+//	public void onClickView(View v) {
+//		switch (v.getId()) {
+//			case R.id.navigation_button:
+//				mCustomDialog = new CustomDialog(this,
+//					leftClickListener,
+//					rightClickListener);
+//					mCustomDialog.show();
+//				break;
+//			case R.id.Messagebox_edit:
+//				input = (EditText)findViewById(R.id.Messagebox_edit);
+//				break;
+//		}
+//	}
+//
+//  private View.OnClickListener rightClickListener = new View.OnClickListener() {
+//		public void onClick(View v) {
+////			final LayoutInflater inflater = (LayoutInflater) getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+////			final View view = inflater.inflate(R.layout.category_add_message_box,null);
+////			final EditText input = (EditText)view.findViewById(R.id.Messagebox_edit);
+//			int result = 0;
+//			String value = input.getText().toString();
+//			System.out.println(value+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+//			String non = "";
+//			result = value.compareTo(non);
+//				if (result == 0) {
+//					Toast.makeText(getApplicationContext(), "제대로좀 쳐라", Toast.LENGTH_SHORT).show();//					mCustomDialog.dismiss();
+//				}
+//				else{;
+//			System.out.println(value+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+//					items.add(" # " + value);
+//					adapter.notifyDataSetChanged();
+//					mCustomDialog.dismiss();
+//				}
+//		}
+//	};
+//
+//	private View.OnClickListener leftClickListener = new View.OnClickListener() {
+//		@Override
+//		public void onClick(View v) {
+//			mCustomDialog.dismiss();
+//		}
+//	};
+
+
 
 
 
