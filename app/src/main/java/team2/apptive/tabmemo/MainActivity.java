0dea523a3eda80404f88ea4a8cbc37264d614942
DarrenKimBat <uiucpass@gmail.com>
2016-12-02 13:17:19 +0900

    리스트뷰 삭제 수정 가능 -DB연결 필요

--- app/src/main/java/team2/apptive/tabmemo/MainActivity.java
+++ app/src/main/java/team2/apptive/tabmemo/MainActivity.java
@@ -96,7 +96,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 		final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 		ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 		//drawer.setDrawerListener(toggle);
-    toggle.getDrawerArrowDrawable();
+    toggle.syncState();
 
 
 		// NavigationView
